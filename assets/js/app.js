5b412f0110e9c5f76e95d6fb22d298f8fea1a8f1
Murtaza34 <murtaza.mohebi@gmail.com>
2023-09-29 11:20:54 +0100

    ajax attempt

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// http://api.alquran.cloud/v1/ayah/262/editions/quran-uthmani,en.asad,en.pickthall
+
+$.ajax({
+  url: "http://api.alquran.cloud/v1/ayah/262/editions/quran-uthmani,en.asad,en.pickthall",
+  method: "GET",
+  success: function (data) {
+    console.log(data);
+  },
+});
+
+
